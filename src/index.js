c8f6df87603b7a39fa950c43ac159eb83bcc44c1
Waqar Mohammad <waqar@waqar.co>
2018-11-30 14:45:41 +0000

    Lessons from Day 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Hi = ({firstName, lastName}) => {
+    return <div>Hello <strong>{firstName} {lastName}</strong>
+        <h1>h1 example</h1>
+        <ul className="list">
+            <li>list 1</li>
+            <li>list 2</li>
+            <li>list 3</li>
+        </ul>
+        {5 + 10}
+    </div>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const MediaCard = ({title, body, imageUrl}) => {
+    return <div>
+                <h2> {title} </h2>
+                <p> {body} </p>
+                <img src={imageUrl} alt="" />
+           </div>
+}
+
+const Gate = ({ isOpen }) => {
+    return <div>
+                <p> { isOpen ? "Open" : "Closed"} </p>
+           </div>
+}
+
+
+ReactDOM.render(<Hi firstName="Waqar" lastName="Mohammad"/>, document.querySelector('#root'));
+ReactDOM.render(<MediaCard title="title" body="body example text" imageUrl="https://via.placeholder.com/300x150"/>, document.querySelector('#root'));
+ReactDOM.render(<Gate isOpen={false}/>, document.querySelector('#root'));
